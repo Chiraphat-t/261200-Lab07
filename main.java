e9565101b273cef041656ca950cf4e8282a5d844
Chiraphat-t <chiraphat.ttt@gmail.com>
2026-01-30 15:31:15 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public void main() {
+    Character A = new Character(100,50);
+    A.swingSword();
+    A.swingSword();A.swingSword();A.swingSword();A.swingSword();A.swingSword();A.swingSword();
+    A.takeDamage(80);
+    A.show();
+    A.takeDamage(80);
+    A.rest();
+    A.show();
+}
